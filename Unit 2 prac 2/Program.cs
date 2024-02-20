67ce3e9bd2dc8f870e1481afb5e7b869d5df166c
drp_3 <drp_3@DESKTOP-T87EQCP>
2024-02-19 23:01:58 -0600

    Agregar archivos de proyecto.

---
+++ Unit 2 prac 2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Converter
+{
+    private const double MilesToKmRatio = 1.60934;
+
+    public double ConvertMilesToKm(double miles)
+    {
+        return miles * MilesToKmRatio;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Converter converter = new Converter();
+        double miles = 10;
+        double km = converter.ConvertMilesToKm(miles);
+        Console.WriteLine("{0} miles is equal to {1} kilometers", miles, km);
+    }
+}
